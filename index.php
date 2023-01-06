d929ad7d64af4c94065e655828db82adca4c8e22
pcjosin <josinchacko2023@cs.ajce.in>
2023-01-06 21:52:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "layouts/header.php"; ?>
+<center>
+<div class="container">
+
+<center><h2 style="color:black; margin-top:20%">Welcome to Just chat.</h2></center>
+</div>
+</center>
+</body>
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Just chat</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <style>
+   
+    .navbar-inverse {
+        background-color: #111111;
+        border-color: #111111; 
+    }
+    .navbar-inverse .navbar-brand {
+        color: white;
+    }
+    .navbar-inverse .navbar-nav>li>a {
+        color: white;
+    }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php">Just chat</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<center>
